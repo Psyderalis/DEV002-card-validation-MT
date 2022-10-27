b19e237e914614eaf135a76fa3858ad7a12268fd
Melanie Tolvett S <melanietolvett@gmail.com>
2022-10-27 13:17:48 -0300

    previo a js

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,32 @@
+function reverseNumber() {
+
+}
+
+function isValid(){
+
+}
+
+function obtenerCCNumber() {
+  let creditCardNumber = document.getElementById("tcNumber").value;
+  console.log("tcNumber");
+} 
+
+//función para validar
+//creditCardNumber.addEventListener("submit", SubmitEvent);
+
+
+
+
+//console.log(creditCardNumber);
+
+
 const validator = {
-  // ...
+  isValid: isValid
 };
 
+
 export default validator;
+
+
+
+
